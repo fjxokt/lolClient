ed5aeaff2bab42e0c90b8030381e430c2e2ee8d5
Emily Mabrey <emilymabrey93+github@gmail.com>
2013-02-26 02:01:42 -0500

    Code fix
    Removed dead code.

--- src/main/java/com/fjxokt/lolclient/utils/SimpleSHA1.java
+++ src/main/java/com/fjxokt/lolclient/utils/SimpleSHA1.java
@@ -32,18 +32,16 @@ public class SimpleSHA1 {
 			Log.getInst().severe("Could not get SHA1 hash!");
 			return null;
 		}
-    	byte[] sha1hash = new byte[40];
 	    md.update(data);
-	    sha1hash = md.digest();
+	    byte[] sha1hash = md.digest();
 	    return convertToHex(sha1hash);
     }
  
     public static String SHA1(String text) throws NoSuchAlgorithmException, UnsupportedEncodingException  { 
 	    MessageDigest md;
 	    md = MessageDigest.getInstance("SHA-1");
-	    byte[] sha1hash = new byte[40];
 	    md.update(text.getBytes("iso-8859-1"), 0, text.length());
-	    sha1hash = md.digest();
+	    byte[] sha1hash = md.digest();
 	    return convertToHex(sha1hash);
     } 
 
